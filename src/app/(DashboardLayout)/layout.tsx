2f854d70f0f5d3b988d7c91fa97485c88ca73547
Md. Zabaidul Islam <zislam5622@gmail.com>
2026-02-02 12:15:22 +0600

    add student dash board

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -1,5 +1,14 @@
-const Dashboardlayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
-};
+import DashboardSidebar from "@/src/components/DashboardSidebar";
 
-export default Dashboardlayout;
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-slate-50">
+      <DashboardSidebar />
+      <main className="flex-grow p-8">{children}</main>
+    </div>
+  );
+}
